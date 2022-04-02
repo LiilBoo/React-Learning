f41659febf544f3865fe1fd45472e5f673bae9bc
LiilB <liltrainer@pm.me>
2022-04-02 11:41:48 +0200

    React using Images

---
+++ React-Use-Images/src/App.js
@@ -0,0 +1,17 @@
+import { Item } from './Components';
+
+function App() {
+ 
+ 
+ 
+ 
+ 
+  return (
+    <div className="App">
+      <h1>Hello I'm a h1 from App component </h1>
+      <Item/>
+    </div>
+  );
+}
+
+export default App;
